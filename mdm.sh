8ab44c8f53c7d25747ad76143ba5333ea2ceddbe
Trung Nguyen <nttrung143@gmail.com>
2020-11-22 15:31:05 +0700

    initial

---
+++ mdm.sh
@@ -0,0 +1,12 @@
+csrutil authenticated-root disable;
+diskutil mount /Volumes/Mac\ OSX;
+ Mount -uv /Volumes/Mac\ OSX;	
+ cd /Volumes/Mac\ OSX/System/Library/LaunchAgents;
+mkdir tmp;
+mv com.apple.ManagedClientAgent.* tmp/;
+mv com.apple.mdmclient.* tmp/;
+cd ../LaunchDaemons;
+mkdir tmp;
+mv com.apple.ManagedClient.* tmp/;
+mv com.apple.mdmclient.* tmp/;
+bless --folder /Volumes/Mac\ OSX/System/Library/CoreServices --bootefi --create-snapshot;
